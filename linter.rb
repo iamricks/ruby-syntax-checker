22494727e8cdf1c2deadff76733ec51f5761b2d6
Ricky <Ricky@MacBook-Pro-2.local>
2021-07-15 11:13:06 -0400

    add script

---
+++ linter.rb
@@ -0,0 +1,11 @@
+# @author ricks
+
+# Find files modified
+# Only works before git add
+modified_file_paths = %x(git diff --name-only).split("\n")
+
+# Loop through files and check syntax, print status
+modified_file_paths.each do |path|
+	next if !path.include?('.rb')
+	print(%x(ruby -c #{path}))
+end
